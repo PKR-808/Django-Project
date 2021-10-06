4bf283f7317511da017f790932c2a8266c86a8fa
PKR-808 <crazybloodmind@gmail.com>
2021-10-06 16:09:56 +0545

    Created models and alerts

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -6,6 +9,7 @@ def index(request):
         'variable':'this is sent',
         'variable1':'cool dude.'
     }
+    messages.success(request, "this is a test message")
     return render(request, 'index.html', context)
 
     #return HttpResponse('this is home page')
@@ -21,5 +25,13 @@ def services(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name =  request.POST.get('name')
+        email =  request.POST.get('email')
+        phone =  request.POST.get('phone')
+        desc =  request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
     return render(request, 'contact.html')
     #return HttpResponse('this is contact page')
